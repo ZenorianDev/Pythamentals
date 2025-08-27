11ce8c86822933ba7a1db1d55a0a3b548268a09d
urhooman <urhooman18@gmail.com>
2025-08-27 23:29:12 +0800

    task1

---
+++ task1.py
@@ -0,0 +1,5 @@
+# Understanding Data Types and Variables
+
+name = input("Enter your name: ")  # String
+age = int(input("Enter your age: "))  # Integer
+height = float(input("Enter your height in meters: "))  # Float
