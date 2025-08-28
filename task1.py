7928d5c94bbfcb40ec457aa8ed4a190ae6374b4a
urhooman <urhooman18@gmail.com>
2025-08-28 23:31:04 +0800

    task1

--- task1.py
+++ task1.py
@@ -3,3 +3,18 @@
 name = input("Enter your name: ")  # String
 age = int(input("Enter your age: "))  # Integer
 height = float(input("Enter your height in meters: "))  # Float
+
+print("Before conversion:")
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+
+
+age = int(age)
+height = float(height)
+
+
+print("\nAfter conversion:")
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
